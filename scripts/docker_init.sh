6ed76060821d515c59c2bc5da48e14d36eb84e28
lucaspsacchi <lucaspsacchi12345@hotmail.com>
2019-05-28 11:34:12 -0300

    Nova tentativa

--- scripts/docker_init.sh
+++ scripts/docker_init.sh
@@ -1,11 +1,11 @@
 
 # Baixando o docker e docker swarn
-sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt -y install docker-ce docker-ce-cli containerd.io
-sudo systemctl start docker
-sudo systemctl enable docker
-# sudo gpasswd -a "${USER}" docker
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update
+apt -y install docker-ce docker-ce-cli containerd.io
+systemctl start docker
+systemctl enable docker
+# gpasswd -a "${USER}" docker
