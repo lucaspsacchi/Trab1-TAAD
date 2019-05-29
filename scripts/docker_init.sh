5017c6c3eb26b79eccaf85a64596fb6a132b451e
lucaspsacchi <lucaspsacchi12345@hotmail.com>
2019-05-29 16:59:13 -0300

    Troca para script externo

--- scripts/docker_init.sh
+++ scripts/docker_init.sh
@@ -1,17 +1,19 @@
-# Teste
+# Atualização padrão
 sudo apt update
 sudo apt -y upgrade
+
+# Clona a pasta do git que tem os script de client/server
 git clone https://github.com/lucaspsacchi/Trab1-TAAD.git
 
 
 # Baixando o docker e docker swarn
-# apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# apt-key fingerprint 0EBFCD88
-# add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-# apt update
-# apt -y install docker-ce docker-ce-cli containerd.io
-# systemctl start docker
-# systemctl enable docker
+apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update
+apt -y install docker-ce docker-ce-cli containerd.io
+systemctl start docker
+systemctl enable docker
 
 # gpasswd -a "${USER}" docker
