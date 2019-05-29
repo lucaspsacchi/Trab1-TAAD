415a0cc81119a1199b3d5a43e64a9868417fed48
Lucas Penteado <lucaspsacchi12345@hotmail.com>
2019-05-29 09:18:06 -0300

    Scripts de client e server UDP em python

---
+++ scripts/client.py
@@ -0,0 +1,17 @@
+# Variáveis globais
+UDP_IP = "127.0.0.1" # IP
+port = 0 # Inicialização da variável
+
+# É uma função para enviar as mensagens UDP
+def client(msg, port):
+
+    # Codifica a mensagem em ASCII e inclui um \0 no final da string
+    info = msg.encode('ascii') + b'\0'
+
+    # Cria o socket UDP
+    sock = socket.socket(socket.AF_INET, # Internet
+                        socket.SOCK_DGRAM) # UDP
+    
+   # Envia a mensagem pelo socket para um ip e uma porta destino
+    sock.sendto(info, (UDP_IP, port))
+
