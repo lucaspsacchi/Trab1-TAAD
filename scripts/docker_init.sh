7be49f3b24827c1f7ebeb93c0c11b6739a254dc2
lucaspsacchi <lucaspsacchi12345@hotmail.com>
2019-05-29 16:41:12 -0300

    Correcao no vagrant

--- scripts/docker_init.sh
+++ scripts/docker_init.sh
@@ -5,12 +5,13 @@ git clone https://github.com/lucaspsacchi/Trab1-TAAD.git
 
 
 # Baixando o docker e docker swarn
-apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt update
-apt -y install docker-ce docker-ce-cli containerd.io
-systemctl start docker
-systemctl enable docker
+# apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# apt-key fingerprint 0EBFCD88
+# add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# apt update
+# apt -y install docker-ce docker-ce-cli containerd.io
+# systemctl start docker
+# systemctl enable docker
+
 # gpasswd -a "${USER}" docker
