58af3953b94ec4a86d49d536bcc08901d0cc077f
lucaspsacchi <lucaspsacchi12345@hotmail.com>
2019-06-06 17:03:48 -0300

    Segunda pagina em flask

--- flask/main.py
+++ flask/main.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
@@ -12,7 +12,12 @@ tasks = [
     }
 ]
 
-@app.route('/tasks', methods=['GET'])
+@app.route('/')
+  def interface():
+    return render_template('interface.html');
+
+# GET exibe lista de todas as tasks
+@app.route('/GET_INFO', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
 
@@ -20,9 +25,22 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-@app.route('/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
+@app.route('/GET_INFO/<int:info>', methods=['GET'])
+def get_task(info):
+    task = [task for task in tasks if task['id'] == info]
     if len(task) == 0:
         abort(404)
     return jsonify({'task': task[0]})
+
+@app.route('/POST_INFO', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
