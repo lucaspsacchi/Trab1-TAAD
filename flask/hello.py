e2ab6a5f9beb75ff7f0577d2c5994a830a505a7f
lucaspsacchi <lucaspsacchi12345@hotmail.com>
2019-06-06 15:57:54 -0300

    Alteracao do dockerfile

--- flask/hello.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__) # Nome da aplicação
-
-@app.route('/<arg>') # Rota raiz
-def hello_world(arg):
-  if not arg:
-    return "Hello World"
-  else:
-    return 'Hello, World!' . format(arg) . 200 # 200 representa o ok
--- flask/rota.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request, render_template
-app = Flask(__name__) # Nome da aplicação
-
-@app.route('/teste') # Rota raiz
-def teste():
-    arg = request.args.get("Argumento");
-    return render_template("/", arg), 200
-
-app.run(use_reloader=True)
