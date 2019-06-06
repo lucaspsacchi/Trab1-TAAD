3b9a47200f4298d889564a07371f3e09be3a31ce
lucaspsacchi <lucaspsacchi12345@hotmail.com>
2019-06-06 16:25:49 -0300

    Segunda pagina em flask

--- flask/main.py
+++ flask/main.py
@@ -18,3 +18,11 @@ def get_tasks():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
