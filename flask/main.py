ec104cca89dff97ef79ae11adb730a42fe6e3a36
lucaspsacchi <lucaspsacchi12345@hotmail.com>
2019-06-06 16:22:37 -0300

    Segunda pagina em flask

--- flask/main.py
+++ flask/main.py
@@ -9,16 +9,10 @@ tasks = [
         'title': u'Buy groceries',
         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
         'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
     }
 ]
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@app.route('/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
 
---
+++ flask/tasks.py
@@ -0,0 +1,8 @@
+from flask import abort
+
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
