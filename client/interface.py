4681a7cf6b03514f061e9ea0f7e510442b9a4901
lucaspsacchi <lucaspsacchi12345@hotmail.com>
2019-06-10 16:00:41 -0300

    Alteração no request

--- client/interface.py
+++ client/interface.py
@@ -34,8 +34,8 @@ while (opcao > 0):
     # Pega as informacoes do docker
     response = requests.get("http://192.168.50.3:5000/" + str(id_container))
     # Passa os dados obtidos para o metodo post do server
-    print("response: " + str(response))
-    response = requests.post("http://192.168.50.2:5000/POST_INFO", data=response)
+    print("response: " + str(response.text))
+    response = requests.post("http://192.168.50.2:5000/POST_INFO", data=response.text)
     if response:
       print("Informacoes inseridas com sucesso!")
   else:
